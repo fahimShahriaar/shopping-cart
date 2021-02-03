96c8e3d4237b27fce2370be4c0171568fcfea12f
fem97 <fem.nwu@gmail.com>
2021-02-03 16:28:45 +0600

    primary level completed

--- script.js
+++ script.js
@@ -7,10 +7,17 @@ document.getElementById('minus-btn-phone').addEventListener('click', function ()
         const priceOfPhone = 1219;
         var totalPhonePrice = priceOfPhone * numberOfPhone;
         document.getElementById('phone-price').innerText = totalPhonePrice;
+
+        // updating subtotal
+        var currentCoverPrice = parseFloat(document.getElementById('cover-price').innerText);
+        var subTotal = currentCoverPrice + totalPhonePrice;
+        document.getElementById('subtotal').innerText = subTotal;
+        document.getElementById('total').innerText = subTotal;
+
     }
 })
 
-// ----- Adding minus button event handler for phone ------
+// ----- Adding plus button event handler for phone ------
 document.getElementById('plus-btn-phone').addEventListener('click', function () {
     var numberOfPhone = parseFloat(document.getElementById('piece-phone').value);
     numberOfPhone += 1;
@@ -18,16 +25,32 @@ document.getElementById('plus-btn-phone').addEventListener('click', function () 
     const priceOfPhone = 1219;
     var totalPhonePrice = priceOfPhone * numberOfPhone;
     document.getElementById('phone-price').innerText = totalPhonePrice;
+
+    // updating subtotal
+    var currentCoverPrice = parseFloat(document.getElementById('cover-price').innerText);
+    var subTotal = currentCoverPrice + totalPhonePrice;
+    document.getElementById('subtotal').innerText = subTotal;
+    document.getElementById('total').innerText = subTotal;
 })
 
 // ----- Adding minus button event handler for cover ------
 document.getElementById('minus-btn-cover').addEventListener('click', function () {
     numberOfCover = parseFloat(document.getElementById('piece-cover').value);
     numberOfCover -= 1;
-    document.getElementById('piece-cover').value = numberOfCover;
-    const priceOfCover = 59;
-    var totalCoverPrice = priceOfCover * numberOfCover;
-    document.getElementById('cover-price').innerText = totalCoverPrice;
+    if (!(numberOfCover < 0)) {
+        document.getElementById('piece-cover').value = numberOfCover;
+        const priceOfCover = 59;
+        var totalCoverPrice = priceOfCover * numberOfCover;
+        document.getElementById('cover-price').innerText = totalCoverPrice;
+
+        // updating subtotal
+        var currentPhonePrice = parseFloat(document.getElementById('phone-price').innerText);
+        var subTotal = currentPhonePrice + totalCoverPrice;
+        document.getElementById('subtotal').innerText = subTotal;
+        document.getElementById('total').innerText = subTotal;
+    }
+
+
 })
 
 // ----- Adding plus button event handler for cover ------
@@ -38,6 +61,14 @@ document.getElementById('plus-btn-cover').addEventListener('click', function () 
     const priceOfCover = 59;
     var totalCoverPrice = priceOfCover * numberOfCover;
     document.getElementById('cover-price').innerText = totalCoverPrice;
+
+
+    // updating subtotal
+    var currentPhonePrice = parseFloat(document.getElementById('phone-price').innerText);
+    var subTotal = currentPhonePrice + totalCoverPrice;
+    document.getElementById('subtotal').innerText = subTotal;
+    document.getElementById('total').innerText = subTotal;
+
 })
 
 // ----- updating payment section -----
